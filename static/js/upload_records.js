82a00b35b1775293de069eaf017a8883a175bde4
hieubkvn123 <javapythonscala@gmail.com>
2022-03-14 00:27:09 +0800

    Added a little bit of animations

--- static/js/upload_records.js
+++ static/js/upload_records.js
@@ -1,3 +1,4 @@
+// For Image uploading
 function preview() {
     frame.src = URL.createObjectURL(event.target.files[0]);
 }
@@ -5,3 +6,8 @@ function clearImage() {
     document.getElementById('formFile').value = null;
     frame.src = "/static/media/xray_placeholder.png";
 }
+
+// For animation
+$(document).ready(() => {
+    $(".fadeIn").fadeIn('slow').removeClass('hidden')
+})
