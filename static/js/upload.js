a622a3d27eda897512c90ec64efb5ec87f11d55e
hieubkvn123 <javapythonscala@gmail.com>
2022-03-13 23:55:34 +0800

    Finished the draft design for create record page

---
+++ static/js/upload.js
@@ -0,0 +1,7 @@
+function preview() {
+    frame.src = URL.createObjectURL(event.target.files[0]);
+}
+function clearImage() {
+    document.getElementById('formFile').value = null;
+    frame.src = "/static/media/xray_placeholder.png";
+}
