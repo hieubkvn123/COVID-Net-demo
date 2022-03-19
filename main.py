8799abe831031332186e04770bbc916e128a3047
KID <kid@kid-2.local>
2022-03-19 15:24:31 +0800

    Created a user list records site

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import hashlib
 import jwt
-import datetime 
+import datetime
 from flask import Flask, request, flash
 from flask import render_template, redirect, url_for, make_response
 
 # Import utilities
-from utils.db import execute_query 
+from utils.db import execute_query
 from utils.tokens import token_required, username_from_token, secret_key, timeout_mins
 from records.routes import records_routes
 
@@ -23,30 +23,30 @@ def login_page():
 	return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
-def login(): 
+def login():
 	if(request.method == 'POST'):
 		# Retrieve data from form
 		account_id = request.form['username']
 		password = request.form['password']
 
-		# Hash the password 
+		# Hash the password
 		hash_password = hashlib.md5(password.encode()).hexdigest()
 
-		# Query the database for account with same username 
+		# Query the database for account with same username
 		rows = execute_query(f"SELECT * FROM ACCOUNT WHERE account_id='{account_id}'")
 
 		# If length of results < 1 - invalid
 		if(len(rows) < 1):
 			flash("Invalid username. Please try again", "danger")
 			return redirect(url_for('login_page'))
-		else: # If username exists 
+		else: # If username exists
 			real_password = rows[0]['password']
 			if(real_password != hash_password):
 				flash("Invalid password. Please try again", "danger")
 				return redirect(url_for("login_page"))
-			else: # Correct password and username 
-				# Generate a JWT token 
-				token = jwt.encode({'username' : account_id, 
+			else: # Correct password and username
+				# Generate a JWT token
+				token = jwt.encode({'username' : account_id,
 					'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=timeout_mins)},
 					key=app.secret_key)
 
@@ -72,6 +72,11 @@ def user_main_page():
 	username = username_from_token(token)
 
 	return render_template('user-create-record.html', **{'username' : username})
+
+@app.route('/test', methods=['GET'])
+@token_required
+def test():
+	return render_template('user-list-records.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True, port=8080)
