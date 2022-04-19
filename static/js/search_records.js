28683d708d86c385a7740eed27c7212b8ed24c2f
hieubkvn123 <javapythonscala@gmail.com>
2022-04-19 16:56:50 +0800

    Completed the read single diagnosis record function

--- static/js/search_records.js
+++ static/js/search_records.js
@@ -21,7 +21,33 @@ jQuery(() => {
         // alert(nric)
         axios.post('/records/get_diagnosis', { nric, datetime }, { 'Content-Type' : 'application/json' })
             .then(res => {
-                console.log(res.data['payload'])
+                let payload = res.data['payload'][0]
+                // let nric = payload['nric_fin'] .
+                // let fname = payload['fname'] .
+                // let lname = payload['lname'] .
+                // let phone = payload['phone'] .
+                // let gender = payload['gender'] .
+                // let dob = payload['dob'] .
+                // let datetime = payload['date_time']
+                // let result = payload['result']
+                // let confidence = payload['confidence']
+                // let xray_url = payload['xray_img_url']
+
+                console.log(payload.date_time)
+
+                // Fill basic particulars
+                $("#info-nric").val(payload.nric_fin)
+                $("#info-fname").val(payload.fname)
+                $("#info-lname").val(payload.lname)
+                $("#info-phone").val(payload.phone)
+                $("#info-gender").val(payload.gender)
+                $("#info-dob").val(payload.dob)
+                $("#info-datetime").val(payload.date_time)
+                $("#info-result").val(payload.result).css("color", payload.result === "negative" ? "green" : "red")
+                $("#info-confidence").val(payload.confidence).css("color", payload.result === "negative" ? "green" : "red")
+
+                // Change image source
+                $("#patient-info-xray-img").attr("src", payload.xray_img_url)
             })
             .catch(err => {
                 if(err.response) 
